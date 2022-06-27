364899e43400a0787278327055289a3bfba72874
Edvard Astapovich <edvardastapovich@gmail.com>
2022-06-27 19:03:12 +0400

    Task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+	let module;
+	let arr1 = [];
+	let arr2 = [];
+
+	if (n < 0) {
+		module = n * -1;
+	} else {
+		module = n;
+	};
+
+	arr1 = String(module).split('');
+
+  	for (let i = arr1.length - 1; i >= 0; i--) {
+		arr2.push(arr1[i]);
+  	}
+
+	return Number(arr2.join(''));
 }
